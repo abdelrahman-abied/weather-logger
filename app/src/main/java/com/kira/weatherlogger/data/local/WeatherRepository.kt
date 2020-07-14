2507231207d44aaed3f1ec44d63abd194550e78a
abdelrahman-abied <abied_2011@yahoo.com>
2020-07-15 01:24:45 +0200

    add weather forecast in room database

--- app/src/main/java/com/kira/weatherlogger/data/local/WeatherRepository.kt
+++ app/src/main/java/com/kira/weatherlogger/data/local/WeatherRepository.kt
@@ -1,8 +1,11 @@
 package com.kira.weatherlogger.data.local
 
+import android.app.Application
+import androidx.annotation.NonNull
 import androidx.lifecycle.LiveData
 
 class WeatherRepository(private val weatherDao: WeatherDao) {
+
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
@@ -11,4 +14,9 @@ class WeatherRepository(private val weatherDao: WeatherDao) {
     suspend fun insert(weatherData: WeatherData) {
         weatherDao.insert(weatherData)
     }
+
+    suspend fun deleteAll() {
+        weatherDao.deleteAll()
+    }
+
 }
--- app/src/main/java/com/kira/weatherlogger/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/kira/weatherlogger/viewmodel/WeatherViewModel.kt
@@ -1,4 +1,32 @@
 package com.kira.weatherlogger.viewmodel
 
-class WeatherViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.kira.weatherlogger.data.local.WeatherData
+import com.kira.weatherlogger.data.local.WeatherRepository
+import com.kira.weatherlogger.data.local.WeatherRoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WeatherViewModel (application: Application) : AndroidViewModel(application)  {
+
+    private val repository: WeatherRepository
+
+    val allWeatherData: LiveData<List<WeatherData>>
+
+    init {
+        val weatherDao = WeatherRoomDatabase.getDatabase(application, viewModelScope).weatherDao()
+        repository = WeatherRepository(weatherDao)
+        allWeatherData = repository.allWeatherData
+    }
+
+
+    fun insert(weatherData: WeatherData) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(weatherData)
+    }
+    fun deleteAll()= viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteAll()
+    }
 }
